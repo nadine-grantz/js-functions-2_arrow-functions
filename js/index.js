fca37a0f258999957e14a38c73abcf33d02fe962
Nadine Grantz <nadine.grantz@googlemail.com>
2024-01-02 23:03:41 +0100

    add solution

--- js/index.js
+++ js/index.js
@@ -19,13 +19,33 @@ function getCurrentHour() {
   }
 }
 
+//Lösungsvorschlag
+
+//function getCurrentHour() => {
+//const now = new Date();
+//const currentHour = now.getHours();
+//if (currentHour === 0) return "12am";
+// else if (currentHour === 12) return "12pm";
+// else if (currentHour <= 11) return currentHour + "am";
+// else return currentHour - 12 + "pm";
+//}
+//------------------------------------------------------------------------
+
 function getVectorLength(x, y, z) {
   return (x ** 2 + y ** 2 + z ** 2) ** 0.5;
 }
 
+//Lösungsvorschlag
+//const getVectorLength = (x, y, z) => (x ** 2 + y ** 2 + z ** 2) ** 0.5;
+//---------------------------------------------------------------------------
+
 function cleanInput(string) {
   return string.toLowerCase().trim();
 }
+
+// Lösungsvorschlag;
+// const cleanInput = (string) => string.toLowerCase().trim();
+//---------------------------------------------------------------------------
 
 /*
 Rewrite the following arrow functions as classic functions.
@@ -43,6 +63,32 @@ const isOddAndSmall = (number) => {
   return true;
 };
 
+// Lösungsvorschlag;
+// function isOddAndSmall(number) {
+//   if (number > 10) {
+//     return false;
+//   }
+
+//   if (number % 2 === 0) {
+//     return false;
+//   }
+
+//   return true;
+// }
+//---------------------------------------------------------------------------
+
 const add3 = (a, b, c) => a + b + c;
 
+// Lösungsvorschlag;
+// function add3(a, b, c) {
+//   return a + b + c;
+// }
+//---------------------------------------------------------------------------
+
 const repeat10 = (string) => string.repeat(10);
+
+// Lösungsvorschlag;
+// function repeat10(string) {
+//   string.repeat(10);
+// }
+// ---------------------------------------------------------------------------
